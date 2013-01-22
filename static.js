9134cbc8b189a998a7fbf63f4dd2dd57b743cd3e
nickleefly <nickleefly@hotmail.com>
2013-01-22 20:33:41 +0800

    added a cacheAndDeliver function

--- static.js
+++ static.js
@@ -7,25 +7,40 @@ var mimeTypes = {
   '.css': 'text/css'
 };
 
+var cache = {};
+function cacheAndDeliver(f, cb) {
+  if(!cache[f]) {
+    fs.readFile(f, function(err, data) {
+      if(!err) {
+        cache[f] = {content: data};
+      }
+      cb(err, data);
+    });
+    return;
+  }
+  console.log('loading' + f + 'from cache');
+  cb(null, cache[f].content);
+}
+
 http.createServer(function(request, response) {
   var lookup = path.basename(decodeURI(request.url)) || 'index.html',
   f = 'content/' + lookup;
   fs.exists(f, function(exists) {
     if(exists) {
-      fs.readFile(f, function(err, data) {
+      cacheAndDeliver(f, function(err, data) {
         if(err) {
           response.writeHead(500);
           response.end('Server Error!');
           return;
         }
 
-        var headers = {'Content-Type': mimeTypes[path.extname(lookup)]};
+        var headers = {'Content-type': mimeTypes[path.extname(lookup)]};
         response.writeHead(200, headers);
         response.end(data);
       });
       return;
     }
-    response.writeHead(400);
-    response.end();
+    response.writeHead(404);
+    response.end('Page Not Found!');
   });
 }).listen(8080);
