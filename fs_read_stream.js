5f0d543b6bb101a59cce50e1d0ab492fe75804a5
nickleefly <nickleefly@hotmail.com>
2013-01-22 22:13:55 +0800

    update createReadStream

--- fs_read_stream.js
+++ fs_read_stream.js
@@ -1,12 +1,24 @@
 var fs = require('fs');
 
 var readStream = fs.createReadStream('/Users/asus/Documents/work/script.js');
+
+readStream.pause();
+
 readStream.on('data', function(data) {
   console.log('got some data:', data);
+  readStream.pause();
+
+  setTimeout(function() {
+    readStream.resume();
+  }, 100);
 });
 
 readStream.on('end', function() {
   console.log('ended');
 });
 
-readStream.setEncoding('utf-8');
+//readStream.setEncoding('base64');
+
+setTimeout(function() {
+  readStream.resume();
+}, 100);
