c516908d4d537c56a5366eb9343b51121c6759c9
nickleefly <nickleefly@hotmail.com>
2013-01-30 20:59:04 +0800

    make cli working

--- file-explorer/cli.js
+++ file-explorer/cli.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var stdin = process.stdin;
 var stdout = process.stdout;
 
-fs.readdir(process.cwd(), function(err, files) {
+fs.readdir(__dirname, function(err, files) {
   console.log('');
 
   if(!files.length) {
@@ -33,35 +33,42 @@ fs.readdir(process.cwd(), function(err, files) {
       }
     });
   }
+  //read user input when files are shown
+  function read() {
+    console.log('');
+    stdout.write('\033[33m Enter your choice: \033[39m');
+  
+    stdin.resume();
+    stdin.setEncoding('utf8');
+    stdin.on('data', option);
+  }
+  
+  //called with the option supplied by the user
+  function option (data) {
+    var filename = files[Number(data)];
+    if (!filename) {
+      stdout.write('   \033[31mEnter your choice: \033[39m');
+    } else {
+      stdin.pause();
+
+      if (stats[Number(data)].isDirectory()) {
+        fs.readdir(__dirname + '/' + filename, function (err, files) {
+          console.log('');
+          console.log('   (' + files.length + ' files)');
+          files.forEach(function (file) {
+            console.log('     -   ' + file);
+          });
+          console.log('');
+        });
+      } else {
+        fs.readFile(__dirname + '/' + filename, 'utf8', function (err, data) {
+          console.log('');
+          console.log('\033[90m' + data.replace(/(.*)/g, '     $1') + '\033[39m');
+        });
+      }
+    }
+  }
+
+  //start by walking the first file
   file(0);
 });
-
-//read user input when files are shown
-function read() {
-  console.log('');
-  stdout.write('\033[33m Enter your choice: \033[39m');
-
-  stdin.resume();
-  stdin.setEncoding('utf8');
-  stdin.on('data', option);
-}
-
-//called with the option supplied by the user
-function option(data) {
-  if(stats[Number(data)].isDirectory()) {
-    fs.readdir(__dirname + '/' + filename, function (err, files) {
-      console.log(''); 
-      console.log('(' + files.length + 'files)');
-      files.forEach(function(files) {
-       console.log('-' + file);
-      });
-      console.log('');
-    });
-  } else {
-    stdin.pause();
-    fs.readFile(__dirname + '/' + filename, 'utf8', function(err, data) {
-      console.log('');
-      console.log('\033[90m' + data.replace(/(.*)/g, '$1') + '\033[39m');
-    });
-  }
-}
