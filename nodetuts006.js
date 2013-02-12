0362bdf9595778f1c869e53dac515d1e720f93c3
nickleefly <nickleefly@hotmail.com>
2013-02-12 17:58:14 +0800

    insert one by one

--- nodetuts006.js
+++ nodetuts006.js
@@ -2,38 +2,36 @@
 insert 10 objects in the database, and when all are done, invoke this callback
 **/
 
+function clone(o) {
+  return JSON.parse(JSON.stringify(o));
+}
+
 function insert(obj, callback) {
   setTimeout(function() {
-    //var error = Math.random() > 0.5 ? new Error('error!!!'):undefined;
-    //callback(error);
+    // var error = Math.random() > 0.5 ? new Error('error!!!'):undefined;
+    // callback(error);
     callback();
   }, Math.random() * 3000);
 }
 
 function insertAll(collection, callback) {
 
+  var queue = clone(collection);
   var finished = 0;
   var calledback = false;
 
-  function done() {
-    if(!calledback) {
-      calledback = true;
-      callback.apply({}, arguments);
-    }
-  }
-
-  for(var i = 0; i < collection.length; i++) {
-    (function(i){
-      insert(collection[i], function(err) {
-        if(err) { return done(err);}
-        console.log('insert %d is finished', i);
-
-        if(++finished === collection.length) {
-          done();
-        }
+  (function insertOne() {
+    var elem = queue.shift();
+    if(elem) {
+      insert(elem, function(err) {
+        if(err) { return callback(err);}
+        console.log('elem %s got inserted', elem);
+        insertOne();
       });
-    }(i));
-  }
+    } else {
+      callback();
+    }
+  }());
 }
 
 var objs = [1,2,3,4,5,6,7,8,9,10];
