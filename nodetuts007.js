5516fbd1c8a2013a3dce1fff32d68f2d84458e71
nickleefly <nickleefly@hotmail.com>
2013-02-12 19:31:12 +0800

    using filter method

--- nodetuts007.js
+++ nodetuts007.js
@@ -1,18 +1,15 @@
 var async = require('async');
 
-function retrieve(id, callback) {
+function filter(obj, callback) {
   setTimeout(function() {
     // var error = Math.random() > 0.5 ? new Error('error!!!'):undefined;
-    callback(null, {
-      id: id,
-      value: Math.round(Math.random() * 1000)
-    });
+    callback(obj >=3);
   }, Math.random() * 1000);
 }
 
 var objects = [1,2,3,4];
 
-async.map(objects, retrieve, function(err, results) {
+async.filter(objects, filter, function(err, results) {
   if(err) { return console.error(err);}
   console.log('Finished results %j', results);
 });
