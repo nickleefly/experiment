83405459981d47fbf28511affd94acb35cd4e352
nickleefly <nickleefly@hotmail.com>
2013-02-12 19:15:00 +0800

    using parallel

--- nodetuts007.js
+++ nodetuts007.js
@@ -2,20 +2,23 @@ var async = require('async');
 
 function insert(obj, callback) {
   setTimeout(function() {
-    //var error = Math.random() > 0.5 ? new Error('error!!!'):undefined;
+    // var error = Math.random() > 0.5 ? new Error('error!!!'):undefined;
     console.log('inserted');
     callback();
   }, Math.random() * 1000);
 }
 
-var itemCount = 100;
-
-var objects = [];
-for(var i = 0; i < itemCount; i++) {
-  objects.push(i);
-}
-
-async.forEachLimit(objects, 10, insert, function(err) {
-  if(err) { return console.error(err);}
-  console.log('Finished');
-})
+async.parallel([
+  function(callback) {
+    insert(1, callback);
+  },
+  function(callback) {
+    insert(2, callback);
+  },
+  function(callback) {
+    insert(3, callback);
+  }],function(err) {
+    if(err) { return console.error(err);}
+    console.log('Finished');
+  }
+)
