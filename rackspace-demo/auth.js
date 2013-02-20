2f0b2794b7321fcdddf1f3cd6958cd21b06220e8
Nick Li <nickleefly@gmail.com>
2013-02-20 22:32:22 +0800

    new commit

--- rackspace-demo/auth.js
+++ rackspace-demo/auth.js
@@ -1,11 +1,15 @@
 var dnode = require('dnode');
-dnode(function(remote, conn) {
+var upnode = require('upnode');
+
+var server = dnode(function(remote, conn) {
   this.auth = function(user, pass, cb) {
     var users = {
       foo: 'bar'
-    }; 
+    };
     var p = users[user];
     if(p === pass) cb(null);
     else cb('ACCESS DENIED');
   };
-}).listen(process.argv[2]);
+});
+server.use(upnode.ping);
+server.listen(process.argv[2]);
