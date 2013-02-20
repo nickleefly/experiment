9b9ad13dcf86ea2e792c3373001e5a7528e81411
nickleefly <nickleefly@gmail.com>
2013-02-20 12:54:43 +0000

    add dnode server

---
+++ rackspace-demo/dnode-server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var dnode = require('dnode');
+var qs = require('querystring');
+
+dnode.connect(8090, function(remote) {
+    http.createServer(function(req, res) {
+    if(req.url === '/login') {
+      var param = qs.parse(req.url.split('?')[1]);
+      remote.auth(param.user, param.pass, function(err) {
+       res.end(err ? err : 'OK!');
+      });
+    }
+    else {
+      res.write('#' + process.argv[3] + '\n');
+      res.end('beep boop!');
+    }
+    }).listen(parseInt(process.argv[2], 10));
+});
