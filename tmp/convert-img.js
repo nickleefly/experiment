34878e4e64de2f88bd65ae0d044b028eba9ab097
nickleefly <nickleefly@hotmail.com>
2013-02-22 19:35:31 +0800

    update using knox s3

--- tmp/convert-img.js
+++ tmp/convert-img.js
@@ -1,12 +1,18 @@
 var http = require('http');
 var spawn = require('child_process').spawn;
+var config = require('./config');
+var s3 = require('knox');
 
 http.createServer(function(req, res) {
   var parts = req.url.split('/');
-  var path = __dirname + '/img/' + parts[1];
-  var args = [path, '-resize', parts[2], '-'];
-  var convert = spawn('convert', args);
 
-  convert.stdout.pipe(res);
-  convert.stderr.pipe(process.stderr);
+  s3.get('/' + parts[1]).on('response', function(s3Res) {
+    var args = ['-', '-resize', parts[2], '-'];
+    var convert = spawn('convert', args);
+
+    s3Res.pipe(convert.stdin);
+
+    convert.stdout.pipe(res);
+    convert.stderr.pipe(process.stderr);
+  }).end();
 }).listen(8000);
