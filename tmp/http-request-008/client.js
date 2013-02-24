16740f1863fe0bf90233ccc45ddaf213f5305484
nickleefly <nickleefly@hotmail.com>
2013-02-24 15:25:10 +0800

    update encoding and one function in server.js

--- tmp/http-request-008/client.js
+++ tmp/http-request-008/client.js
@@ -5,24 +5,17 @@ var options = {
   port: 8080,
   path: '/path',
   headers: {
-    'X-Powered-By': 'Node',
-    'X-Author': 'Nick'
+   'Transfer-Encoding': 'chunked'
   }
 };
 
-http.globalAgent.maxSockets = 10;
+var client = http.request(options);
 
-function fireRequest() {
-  var client = http.request(options);
+client.on('response', function(res) {
+console.log('status code: %d', res.statusCode);
+console.log('response headers: %j', res.headers);
+});
 
-  client.on('response', function(res) {
-    console.log('status code: %d', res.statusCode);
-    console.log('response headers: %j', res.headers);
-  });
-
-  client.end();
-}
-
-for(var i = 0; i < 20; i++) {
-  fireRequest();
-}
+client.write('body part 1');
+client.write('body part 2');
+client.end('body part 3');
--- tmp/http-request-008/server.js
+++ tmp/http-request-008/server.js
@@ -3,18 +3,20 @@ var http = require('http');
 var concurrentRequests = 0;
 
 http.createServer(function(req, res) {
-  console.log('concurrent requests: %d', ++concurrentRequests);
+  //console.log('concurrent requests: %d', ++concurrentRequests);
+
   console.log('new request -> ');
   console.log(' url: %s', req.url);
   console.log(' headers: %j', req.headers);
 
-  req.setEncoding('urf8');
+  req.setEncoding('utf8');
+
   req.on('data', function(data) {
     console.log(' data: %s', data); 
   });
 
-  setTimeout(function() {
-    console.log('concurrent requests: %d', --concurrentRequests);
-    res.end('Hello');
-  }, 1000);
+  req.on('end', function() {
+  });
+
+  res.end('Hello');
 }).listen(8080);
