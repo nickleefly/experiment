f895c4afc0fbd3fbfe4e385a32d497c5ff119f03
nickleefly <nickleefly@hotmail.com>
2013-02-24 16:02:48 +0800

    update client

--- tmp/http-request-008/client.js
+++ tmp/http-request-008/client.js
@@ -2,10 +2,12 @@ var http = require('http');
 var fs = require('fs');
 
 var readStream = fs.createReadStream('./cat.jpg');
+var ws = fs.createWriteStream('./cat_back.jpg');
 
 var options = {
   host: 'localhost',
   port: 8080,
+  method: 'POST',
   path: '/path',
   headers: {
    'Transfer-Encoding': 'chunked'
@@ -15,8 +17,9 @@ var options = {
 var req = http.request(options);
 
 req.on('response', function(res) {
-console.log('status code: %d', res.statusCode);
-console.log('response headers: %j', res.headers);
+  console.log('status code: %d', res.statusCode);
+  console.log('response headers: %j', res.headers);
+  res.pipe(ws);
 });
 
 readStream.pipe(req);
--- tmp/http-request-008/server.js
+++ tmp/http-request-008/server.js
@@ -1,19 +1,12 @@
 var http = require('http');
 var fs = require('fs');
 
-var concurrentRequests = 0;
-
 http.createServer(function(req, res) {
-  var ws = fs.createWriteStream('./cat_copy.jpg');
 
   console.log('new request -> ');
+  console.log(' method: %s', req.method);
   console.log(' url: %s', req.url);
   console.log(' headers: %j', req.headers);
 
-  req.pipe(ws);
-
-  req.on('end', function() {
-    res.end('Hello');
-  });
-
+  req.pipe(res);
 }).listen(8080);
