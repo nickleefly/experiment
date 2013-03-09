834942f1f25f00966bb44f10a190a67a827d8a42
nickleefly <nickleefly@hotmail.com>
2013-03-09 18:37:49 +0800

    add comment

--- mongoose/example.js
+++ mongoose/example.js
@@ -1,15 +1,21 @@
 var mongoose = require('mongoose');
     Schema   = mongoose.Schema;
 
+var CommentSchema = new Schema({
+  email: String,
+  body: String
+})
+
 var PostSchema = new Schema({
   title: String,
   body: String,
   date: {type: Date, default: Date.now()},
   state: {type: String, enum: ['draft', 'published', 'private'], default: 'draft'},
   author: {
-  name: String,
-  email: {type: String, validate: /^[A-Za-z0-9._+\-\']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$/i}
-  }
+    name: String,
+    email: {type: String, validate: /^[A-Za-z0-9._+\-\']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$/i}
+  },
+  comments: [CommentSchema]
 });
 
 mongoose.connect('mongodb://localhost/test');
@@ -23,6 +29,7 @@ post.body = 'Post body';
 post.state = 'published';
 //post.date = Date.now();
 post.author = {name: 'Nick', email: 'nickleefly@hotmail.com'};
+post.comments.push({email:'a@b.com', body: 'beep boop'});
 
 post.save(function(err) {
   if (err) {throw err;}
