bdc69e0b02a5a8a6b4673db9453743672ed97801
nickleefly <nickleefly@hotmail.com>
2013-06-11 15:53:26 +0800

    update http

--- express/http.js
+++ express/http.js
@@ -3,12 +3,19 @@ var http = require("http");
 
 // Build the server
 var app = http.createServer(function(request, response) {
-  response.writeHead(200, {
-	  "Content-Type": "text/plain"
-	});
-	response.end("Hello world!\n");
+
+  // Build the answer
+  var answer = "";
+  answer += "Request URL: " + request.url + "\n";
+  answer += "Request type: " + request.method + "\n";
+  answer += "Request headers: " + JSON.stringify(request.headers) + "\n";
+
+  // Send answer
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end(answer);
+
 });
 
 // Start that server, baby
-app.listen(1337, "localhost");
+app.listen(3000, "localhost");
 console.log("Server running at http://localhost:1337/");
