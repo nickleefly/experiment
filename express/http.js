b9f2c51e14021b99550c316707acf36f705e7ffa
nickleefly <nickleefly@hotmail.com>
2013-06-11 16:06:15 +0800

    add express
    this version is using express middleware
    having router for home page, about page and 404

--- express/http.js
+++ express/http.js
@@ -1,35 +1,22 @@
-var connect = require("connect");
+var express = require("express");
 var http = require("http");
-var app = connect();
+var app = express();
 
-app.use(connect.logger());
-
-// Homepage
-app.use(function(request, response, next) {
-  if (request.url == "/") {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Welcome to the homepage!\n");
-    // The middleware stops here.
-  } else {
-    next();
-  }
-});
-
-// About page
-app.use(function(request, response, next) {
-  if (request.url == "/about") {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Welcome to the about page!\n");
-    // The middleware stops here.
-  } else {
-    next();
-  }
-});
-
-// 404'd!
-app.use(function(request, response) {
+app.all("*", function(request, response, next) {
   response.writeHead(404, { "Content-Type": "text/plain" });
-  response.end("404 error!\n");
+  next();
+});
+
+app.get("/", function(request, response) {
+  response.end("Welcome to the homepage!");
+});
+
+app.get("/about", function(request, response) {
+  response.end("Welcome to the about page!");
+});
+
+app.get("*", function(request, response) {
+  response.end("404!");
 });
 
 http.createServer(app).listen(1337);
