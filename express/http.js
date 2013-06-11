41e9ca6fdb9083eaf916dad5df4db46bd8b9cf03
nickleefly <nickleefly@hotmail.com>
2013-06-11 15:59:03 +0800

    add connect middleware

--- express/http.js
+++ express/http.js
@@ -1,23 +1,15 @@
+// Require the stuff we need
+var connect = require("connect");
 var http = require("http");
 
-http.createServer(function(req, res) {
+// Build the app
+var app = connect();
 
-  // Homepage
-  if (req.url == "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Welcome to the homepage!");
-  }
+// Add some middleware
+app.use(function(request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hello world!\n");
+});
 
-  // About page
-  else if (req.url == "/about") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Welcome to the about page!");
-  }
-
-  // 404'd!
-  else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 error! File not found.");
-  }
-
-}).listen(1337, "localhost");
+// Start it up!
+http.createServer(app).listen(1337);
