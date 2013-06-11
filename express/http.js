ae5f7d8f2a9e670fcb7cf3e9035391f954e4acf5
nickleefly <nickleefly@hotmail.com>
2013-06-11 16:02:36 +0800

    add more

--- express/http.js
+++ express/http.js
@@ -3,11 +3,33 @@ var http = require("http");
 var app = connect();
 
 app.use(connect.logger());
-// Fun fact: connect.logger() returns a function.
 
+// Homepage
+app.use(function(request, response, next) {
+  if (request.url == "/") {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Welcome to the homepage!\n");
+    // The middleware stops here.
+  } else {
+    next();
+  }
+});
+
+// About page
+app.use(function(request, response, next) {
+  if (request.url == "/about") {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Welcome to the about page!\n");
+    // The middleware stops here.
+  } else {
+    next();
+  }
+});
+
+// 404'd!
 app.use(function(request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello world!\n");
+  response.writeHead(404, { "Content-Type": "text/plain" });
+  response.end("404 error!\n");
 });
 
 http.createServer(app).listen(1337);
