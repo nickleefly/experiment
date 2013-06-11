7b69b18b797bd83107a182e91d5eaadd61236c07
nickleefly <nickleefly@hotmail.com>
2013-06-11 15:56:13 +0800

    add routers

--- express/http.js
+++ express/http.js
@@ -1,21 +1,23 @@
-// Require what we need
 var http = require("http");
 
-// Build the server
-var app = http.createServer(function(request, response) {
+http.createServer(function(req, res) {
 
-  // Build the answer
-  var answer = "";
-  answer += "Request URL: " + request.url + "\n";
-  answer += "Request type: " + request.method + "\n";
-  answer += "Request headers: " + JSON.stringify(request.headers) + "\n";
+  // Homepage
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Welcome to the homepage!");
+  }
 
-  // Send answer
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end(answer);
+  // About page
+  else if (req.url == "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Welcome to the about page!");
+  }
 
-});
+  // 404'd!
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 error! File not found.");
+  }
 
-// Start that server, baby
-app.listen(3000, "localhost");
-console.log("Server running at http://localhost:1337/");
+}).listen(1337, "localhost");
