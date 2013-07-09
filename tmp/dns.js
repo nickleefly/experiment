8f42391dc6678353d256320023bb81f58b1518c5
nickleefly <nickleefly@hotmail.com>
2013-07-09 08:46:56 +0800

    clean up

--- tmp/dns.js
+++
@@ -1,6 +0,0 @@
-var dns = require('dns');
-
-dns.lookup('google.com', function(err, ip) {
-  if(err) return handleError(err);
-  console.log('google.com resolved to %s', ip);
-});
--- tmp/superagent.js
+++
@@ -1,6 +0,0 @@
-var request = require('superagent');
-
-request.get('http://twitter.com/search.json')
-  .send({q : 'justin bieber'})
-  .set(Date, new Date)
-  .end(function(res) { console.log(res.body);});
