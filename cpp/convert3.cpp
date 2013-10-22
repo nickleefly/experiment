a4cad050207b06908df11741491e3607389d5e5a
nickleefly <nickleefly@gmail.com>
2013-10-22 22:45:13 +0800

    indent and comment

--- cpp/convert3.cpp
+++ cpp/convert3.cpp
@@ -5,27 +5,26 @@ using namespace std;
 const int N=40;
 
 template <class T>
-/*universal function sum : add the d table values
-  inputs : reference to T p
-               int n
-               int d[]
-*/
+//universal function sum : add the d table values
+//inputs : reference to T p
+//int n
+//int d[]
 inline void sum(T& p, T n, T d[])
 {
-    for(T i = 0; i < n; ++i)
-      p += d[i];
+  for(T i = 0; i < n; ++i)
+    p += d[i];
 }
 
 int main()
 {
-    int accum = 0;  //or int accum(0);
-    int data[N];
+  int accum = 0;  //or int accum(0);
+  int data[N];
 
-    //we fill the d table
-    for(int i = 0; i < N; ++i) // i exists only in the for
-      data[i] = i;
+  //we fill the d table
+  for(int i = 0; i < N; ++i) // i exists only in the for
+    data[i] = i;
 
-    sum(accum, N, data);
-    cout << "sum is " << accum << endl;
-    return 0;
+  sum(accum, N, data);
+  cout << "sum is " << accum << endl;
+  return 0;
 }
