557e63f0efa6722099a1838f585bb00e64eb4c23
nickleefly <nickleefly@gmail.com>
2013-10-22 23:03:26 +0800

    udpate sum

--- cpp/sum.cpp
+++ cpp/sum.cpp
@@ -13,8 +13,7 @@ template <class T> //T is generic type
 T sum(const T data[], int size, T s = 0)
 {
   for(int i = 0; i < size; ++i)
-    //s += data[i]; //+= must work for T
-    cout << "data[" << i << "] is " << data[i] << endl;
+    s += data[i]; //+= must work for T
   return s;
 }
 
@@ -23,10 +22,7 @@ int main()
   cout << "template for sum()" << endl;
   int a[] = {1, 2, 3};
   double b[] = {2.1, 2.2, 2.3};
-  sum(a, 3);
-  cout << "\n" << endl;
-  sum(b, 3);
-  //cout << sum(a, 3) << endl;
-  //cout << sum(b, 3) << endl;
+  cout << sum(a, 3) << endl;
+  cout << sum(b, 3) << endl;
   return 0;
 }
