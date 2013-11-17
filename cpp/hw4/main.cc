99f2feecc4ead80a75580ff9ecc2ed67a75b5379
nickleefly <nickleefly@gmail.com>
2013-11-17 22:54:54 +0800

    add hw4 solution

---
+++ cpp/hw4/main.cc
@@ -0,0 +1,13 @@
+// Description: HW4 the HEX program
+// c++11
+// to compile: g++ -std=c++11 graph.cc shortest_path.cc main.cc;
+#include <iostream>
+
+#include "graph.h"
+#include "shortest_path.h"
+
+using namespace std;
+
+int main(){
+  cout << "HW 4" << endl;
+}
